24ea33b7df304e1b65f1842d0633453e3740db39
MagalGabriel <gabrielmaaga@gmail.com>
2023-10-19 18:58:22 -0300

    Modificação de imagens da Home e Implementação do fragment da barra de navegação

---
+++ app/src/main/java/edu/example/pi/BarraAtalhosFragment.java
@@ -0,0 +1,26 @@
+package edu.example.pi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BarraAtalhosFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState){
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.barra_atalhos, container, false);
+    }
+
+
+
+}
