6138fdabfa3f1b9a1ef5d5afd95cd29ba98bf1ea
Matheus3788 <matheus.carsimoes@gmail.com>
2023-11-05 00:25:31 -0300

    Modal

--- app/src/main/java/edu/example/pi/EditProfile.java
+++ app/src/main/java/edu/example/pi/EditProfile.java
@@ -1,15 +1,45 @@
 package edu.example.pi;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditProfile extends AppCompatActivity {
 
+    Button btnatualizar;
+    Button btnexcliur;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_profile_layout);
+
+        btnexcliur = findViewById(R.id.btnexcluir);
+
+        btnexcliur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modalconfirmacao();
+            }
+        });
+
     }
+
+    void  modalconfirmacao(){
+        final Dialog dialog = new Dialog(EditProfile.this);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.modal_confirmar_exclusao);
+        dialog.show();
+    }
+
+
+
+
 }
