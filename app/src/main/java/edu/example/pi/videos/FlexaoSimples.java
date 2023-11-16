ab1b91d3d975d7ae9bd42ee6d223718d4d9a2dd4
Giovanna B. dos Santos <giovanna.berato@gmail.com>
2023-11-16 18:53:02 -0300

    Membros Superiores

---
+++ app/src/main/java/edu/example/pi/videos/FlexaoSimples.java
@@ -0,0 +1,53 @@
+package edu.example.pi.videos;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.example.pi.MembrosSuperiores;
+import edu.example.pi.R;
+
+public class FlexaoSimples extends AppCompatActivity {
+
+    private VideoView videoView;
+    private ImageButton btnvoltar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercicios_flexao_simples);
+        btnvoltar = findViewById(R.id.btnvoltar);
+
+        videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName()+"/" + R.raw.flexao_simples;
+
+
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        videoView.start();
+
+        btnvoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FlexaoSimples.this, MembrosSuperiores.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+}
